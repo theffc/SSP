b241be11ba0db17a6efe35dea68707a8de0debc4
theffc <frederico.calhau@zup.com.br>
2020-08-29 11:45:58 -0300

    extract FillTemplateParameters from EarlyAlertService

--- src/main/java/org/jasig/ssp/service/impl/EarlyAlertServiceImpl.java
+++ src/main/java/org/jasig/ssp/service/impl/EarlyAlertServiceImpl.java
@@ -19,7 +19,6 @@
 package org.jasig.ssp.service.impl; // NOPMD by jon.adams
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import org.apache.commons.lang.StringUtils;
 import org.jasig.ssp.dao.EarlyAlertDao;
 import org.jasig.ssp.factory.EarlyAlertSearchResultTOFactory;
@@ -31,12 +30,9 @@ import org.jasig.ssp.model.ObjectStatus;
 import org.jasig.ssp.model.Person;
 import org.jasig.ssp.model.PersonProgramStatus;
 import org.jasig.ssp.model.SubjectAndBody;
-import org.jasig.ssp.model.external.FacultyCourse;
-import org.jasig.ssp.model.external.Term;
 import org.jasig.ssp.model.reference.Campus;
 import org.jasig.ssp.model.reference.EarlyAlertReason;
 import org.jasig.ssp.model.reference.EarlyAlertSuggestion;
-import org.jasig.ssp.model.reference.EnrollmentStatus;
 import org.jasig.ssp.model.reference.ProgramStatus;
 import org.jasig.ssp.model.reference.StudentType;
 import org.jasig.ssp.security.SspUser;
@@ -48,20 +44,15 @@ import org.jasig.ssp.service.ObjectNotFoundException;
 import org.jasig.ssp.service.PersonProgramStatusService;
 import org.jasig.ssp.service.PersonService;
 import org.jasig.ssp.service.SecurityService;
-import org.jasig.ssp.service.external.FacultyCourseService;
-import org.jasig.ssp.service.external.TermService;
 import org.jasig.ssp.service.reference.ConfigService;
 import org.jasig.ssp.service.reference.EarlyAlertReasonService;
 import org.jasig.ssp.service.reference.EarlyAlertSuggestionService;
-import org.jasig.ssp.service.reference.EnrollmentStatusService;
 import org.jasig.ssp.service.reference.MessageTemplateService;
 import org.jasig.ssp.service.reference.ProgramStatusService;
 import org.jasig.ssp.service.reference.StudentTypeService;
 import org.jasig.ssp.transferobject.EarlyAlertSearchResultTO;
 import org.jasig.ssp.transferobject.PagedResponse;
 import org.jasig.ssp.transferobject.form.EarlyAlertSearchForm;
-import org.jasig.ssp.transferobject.messagetemplate.CoachPersonLiteMessageTemplateTO;
-import org.jasig.ssp.transferobject.messagetemplate.EarlyAlertMessageTemplateTO;
 import org.jasig.ssp.transferobject.reports.EarlyAlertCourseCountsTO;
 import org.jasig.ssp.transferobject.reports.EarlyAlertReasonCountsTO;
 import org.jasig.ssp.transferobject.reports.EarlyAlertStudentReportTO;
@@ -140,14 +131,6 @@ public class EarlyAlertServiceImpl extends // NOPMD
 
 	// 1
 	@Autowired
-	private transient FacultyCourseService facultyCourseService;
-
-	// 1
-	@Autowired
-	private transient TermService termService;
-
-	// 1
-	@Autowired
 	private transient PersonProgramStatusService personProgramStatusService;
 
 	// 1
@@ -165,10 +148,6 @@ public class EarlyAlertServiceImpl extends // NOPMD
 	// 1
 	@Autowired
 	private transient EarlyAlertSearchResultTOFactory searchResultFactory;
-
-	// 1
-	@Autowired
-	private transient EnrollmentStatusService enrollmentStatusService;
 
 	// 2
 	private static final Logger LOGGER = LoggerFactory
@@ -641,7 +620,7 @@ public class EarlyAlertServiceImpl extends // NOPMD
 		}
 
 		// 1
-        if (configService.getByNameOrDefaultValue("send_faculty_mail") != true) {
+        if (!configService.getByNameOrDefaultValue("send_faculty_mail")) {
             LOGGER.debug("Skipping Faculty Early Alert Confirmation Email: Config Turned Off");
             return; //skip if faculty early alert email turned off
         }
@@ -667,169 +646,8 @@ public class EarlyAlertServiceImpl extends // NOPMD
 	@Override
 	public Map<String, Object> fillTemplateParameters(
 			@NotNull final EarlyAlert earlyAlert) {
-		// 1
-		if (earlyAlert == null) {
-			throw new IllegalArgumentException("EarlyAlert was missing.");
-		}
-
-		// 1
-		if (earlyAlert.getPerson() == null) {
-			throw new IllegalArgumentException("EarlyAlert.Person is missing.");
-		}
-
-		// 1
-		if (earlyAlert.getCreatedBy() == null) {
-			throw new IllegalArgumentException(
-					"EarlyAlert.CreatedBy is missing.");
-		}
-
-		// 1
-		if (earlyAlert.getCampus() == null) {
-			throw new IllegalArgumentException("EarlyAlert.Campus is missing.");
-		}
-
-		// ensure earlyAlert.createdBy is populated
-		// 1
-		if ((earlyAlert.getCreatedBy() == null)
-				|| (earlyAlert.getCreatedBy().getFirstName() == null)) {
-			// 1
-			if (earlyAlert.getCreatedBy() == null) {
-				throw new IllegalArgumentException(
-						"EarlyAlert.CreatedBy is missing.");
-			}
-		}
-
-		final Map<String, Object> templateParameters = Maps.newHashMap();
-
-		final String courseName = earlyAlert.getCourseName();
-		// 1
-		if ( StringUtils.isNotBlank(courseName) ) {
-			Person creator;
-			// 2
-			try {
-				creator = personService.get(earlyAlert.getCreatedBy().getId());
-			} catch (ObjectNotFoundException e1) {
-				throw new IllegalArgumentException(
-						"EarlyAlert.CreatedBy.Id could not be loaded.", e1);
-			}
-			final String facultySchoolId = creator.getSchoolId();
-			// 1
-			if ( (StringUtils.isNotBlank(facultySchoolId)) ) {
-				String termCode = earlyAlert.getCourseTermCode();
-				FacultyCourse course = null;
-				// 2
-				try {
-					// 2
-					if ( StringUtils.isBlank(termCode) ) {
-						course = facultyCourseService.
-								getCourseByFacultySchoolIdAndFormattedCourse(
-										facultySchoolId, courseName);
-					} else {
-						course = facultyCourseService.
-								getCourseByFacultySchoolIdAndFormattedCourseAndTermCode(
-										facultySchoolId, courseName, termCode);
-					}
-				} catch ( ObjectNotFoundException e ) {
-					// Trace irrelevant. see below for logging. prefer to
-					// do it there, after the null check b/c not all service
-					// methods implement ObjectNotFoundException reliably.
-				}
-
-				// 1
-				if ( course != null ) {
-					templateParameters.put("course", course);
-					// 1
-					if ( StringUtils.isBlank(termCode) ) {
-						termCode = course.getTermCode();
-					}
-					// 1
-					if ( StringUtils.isNotBlank(termCode) ) {
-						Term term = null;
-						// 2
-						try {
-							term = termService.getByCode(termCode);
-						} catch ( ObjectNotFoundException e ) {
-							// Trace irrelevant. See below for logging.
-						}
-						// 2
-						if ( term != null ) {
-							templateParameters.put("term", term);
-						} else {
-							LOGGER.info("Not adding term to message template"
-									+ " params or early alert {} because"
-									+ " the term code {} did not resolve to"
-									+ " an external term record",
-									earlyAlert.getId(), termCode);
-						}
-					}
-				}
-				// 1
-				else {
-					LOGGER.info("Not adding course nor term to message template"
-							+ " params for early alert {} because the associated"
-							+ " course {} and faculty school id {} did not"
-							+ " resolve to an external course record.",
-							new Object[] { earlyAlert.getId(), courseName,
-									facultySchoolId});
-				}
-			}
-		}
-		Person creator = null;
-		// 2
-		try{
-			creator = personService.get(earlyAlert.getCreatedBy().getId());
-		}catch(ObjectNotFoundException exp)	{
-			LOGGER.error("Early Alert Creator Not found sending message for early alert:" + earlyAlert.getId(), exp);
-		}
-		// 1
-		EarlyAlertMessageTemplateTO eaMTO = new EarlyAlertMessageTemplateTO(earlyAlert,creator);
-		
-		//Only early alerts response late messages sent to coaches
-		// 1
-		if(eaMTO.getCoach() == null){
-			// 2
-			try{
-				// if no earlyAlert.getCampus()  error thrown by design, should never not be a campus.
-				eaMTO.setCoach(new CoachPersonLiteMessageTemplateTO(personService.get(earlyAlert.getCampus().getEarlyAlertCoordinatorId())));
-			}catch(ObjectNotFoundException exp){
-				LOGGER.error("Early Alert with id: " + earlyAlert.getId() + " does not have valid campus coordinator, no coach assigned: " + earlyAlert.getCampus().getEarlyAlertCoordinatorId(), exp);
-			}
-		}
-		
-		String statusCode = eaMTO.getEnrollmentStatus();
-		// 1
-		if(statusCode != null) {
-			EnrollmentStatus enrollmentStatus = enrollmentStatusService.getByCode(statusCode);
-			// 1
-			if(enrollmentStatus != null) {
-				
-				//if we have made it here... we can add the status!
-				templateParameters.put("enrollment", enrollmentStatus);
-			}
-		}
-		
-		
-		
-		
-		templateParameters.put("earlyAlert", eaMTO);
-		templateParameters.put("termToRepresentEarlyAlert",
-				configService.getByNameEmpty("term_to_represent_early_alert"));
-		templateParameters.put("TermToRepresentEarlyAlert",
-				configService.getByNameEmpty("term_to_represent_early_alert"));
-		templateParameters.put("termForEarlyAlert",
-				configService.getByNameEmpty("term_to_represent_early_alert"));
-		templateParameters.put("linkToSSP",
-				configService.getByNameEmpty("serverExternalPath"));
-		templateParameters.put("applicationTitle",
-				configService.getByNameEmpty("app_title"));
-		templateParameters.put("institutionName",
-				configService.getByNameEmpty("inst_name"));
-		
-		templateParameters.put("FirstName", eaMTO.getPerson().getFirstName());
-		templateParameters.put("LastName", eaMTO.getPerson().getLastName());
-		templateParameters.put("CourseName", eaMTO.getCourseName());
-
-		return templateParameters;
+		FillTemplateParameters filler = new FillTemplateParameters();
+		return filler.fill(earlyAlert);
 	}
 
 	@Override
